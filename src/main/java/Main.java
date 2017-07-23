61901535c2c25f699a1690b6778f9002bb95f1ea
zzhang115 <zzhang115@dons.usfca.edu>
2017-07-22 21:47:35 -0700

    add mapreducer builder

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+/**
+ * Created by zzc on 7/22/17.
+ */
+public class Main
+{
+}
--- src/main/java/NGramLibraryBuilder.java
+++ src/main/java/NGramLibraryBuilder.java
@@ -1,3 +1,4 @@
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -16,13 +17,33 @@ public class NGramLibraryBuilder
         @Override
         protected void setup(Context context) throws IOException, InterruptedException
         {
-            super.setup(context);
+
         }
 
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
         {
-            super.map(key, value, context);
+            //input a sentence
+            //I love bigdata n = 3
+            /* I love -> 1
+            love bigdata -> 1
+            I love big -> 1
+            */
+            Configuration conf = context.getConfiguration();
+            int numGram = conf.getInt("numGram", 5);// 5 is default value, if get value is null
+            String line = value.toString().trim().toLowerCase().replaceAll("[^a-z]", " ");
+            String[] words = line.split("\\s+");
+            if(words.length < 2)
+            {
+                return;
+            }
+            StringBuilder stringBuilder;
+            for(int i = 0; i < words.length; i++)
+            {
+                stringBuilder = new StringBuilder();
+                stringBuilder.append(words[i]);
+
+            }
         }
     }
     public static class NGramReducer extends Reducer<Text, IntWritable, Text, IntWritable>
