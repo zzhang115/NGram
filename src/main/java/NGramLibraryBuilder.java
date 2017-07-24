187230805d19f9775e473782f74231db4498d37f
zzhang115 <zzhang115@dons.usfca.edu>
2017-07-23 23:04:41 -0700

    add mapreducer builder

--- src/main/java/NGramLibraryBuilder.java
+++ src/main/java/NGramLibraryBuilder.java
@@ -4,7 +4,6 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-
 import java.io.IOException;
 
 /**
