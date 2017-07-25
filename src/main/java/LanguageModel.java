c2e701fb8d73fc6391150d6da1daf38341592388
zzhang115 <zzhang115@dons.usfca.edu>
2017-07-24 21:54:17 -0700

    add DBWritable

--- src/main/java/LanguageModel.java
+++ src/main/java/LanguageModel.java
@@ -3,6 +3,7 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 
