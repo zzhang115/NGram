a5570dfb09da01ac5c9e3e9ed258b4e6ebae16de
zzhang115 <zzhang115@dons.usfca.edu>
2017-07-24 21:47:52 -0700

    add second mapreducer

--- src/main/java/LanguageModel.java
+++ src/main/java/LanguageModel.java
@@ -1,3 +1,4 @@
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
@@ -14,6 +15,14 @@ public class LanguageModel
     {
         // input: I love big data \t 10
         // output: key = I love big value = data=10
+        private int threshold;
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException
+        {
+            Configuration configuration = context.getConfiguration();
+            threshold = configuration.getInt("threshold", 20);
+
+        }
 
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
@@ -25,7 +34,20 @@ public class LanguageModel
                 return;
             }
             String[] words = wordsPlusCount[0].split("\\s+");
-
+            int count = Integer.parseInt(wordsPlusCount[1]);
+            if(count < threshold)
+            {
+                return;
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int i = 0; i < words.length - 1; i++)
+            {
+                stringBuilder.append(words[i]);
+                stringBuilder.append(" ");
+            }
+            String outputKey = stringBuilder.toString().trim();
+            String outputValue = words[words.length - 1] + "=" + count;
+            context.write(new Text(outputKey), new Text(outputValue));
         }
     }
     public static class Reduce extends Reducer<Text, Text, DBOutputWritable, NullWritable>
