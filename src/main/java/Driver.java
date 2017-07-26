25f49544956ee69c9ddd7172196b93625033226d
zzhang115 <zzhang115@dons.usfca.edu>
2017-07-25 22:08:14 -0700

    add word count delete directory

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.lib.db.DBConfiguration;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.db.DBOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
@@ -20,11 +21,6 @@ public class Driver
 {
     public static void main(String[] args) throws ClassNotFoundException, IOException, InterruptedException
     {
-        //inputDir
-        //outputDir
-        //NGram
-        //threshold
-        //topK
         String inputDir = args[0];
         String outputDir = args[1];
         String nGrams= args[2];
@@ -59,33 +55,34 @@ public class Driver
         TextOutputFormat.setOutputPath(job1, new Path(outputDir));
         job1.waitForCompletion(true);
 
-        //connect two jobs, last output is the next input
+        // connect two jobs, last output is the next input
         // job2
-//        Configuration configuration2 = new Configuration();
-//        configuration2.set("threshold", threshold);
-//        configuration2.set("n", topK);
-//
-//        DBConfiguration.configureDB(configuration2, "com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/test", "root", "238604");
-//        Job job2 = Job.getInstance(configuration2);
-//        job2.setJobName("Model");
-//        job2.setJarByClass(Driver.class);
-//
-////        job2.addArchiveToClassPath(new Path("path_to_ur_connector"));
-//        job2.setMapperClass(LanguageModel.Map.class);
-//        job2.setReducerClass(LanguageModel.Reduce.class);
-//
-//        job2.setMapOutputKeyClass(Text.class);
-//        job2.setMapOutputValueClass(Text.class);
-//        job2.setOutputKeyClass(DBOutputWritable.class);
-//        job2.setOutputValueClass(NullWritable.class);
-//
-//        job2.setInputFormatClass(TextInputFormat.class);
-//        job2.setOutputFormatClass(DBOutputFormat.class);
-//
-//        DBOutputFormat.setOutput(job2, "output", new String[] {"starting_phrase", "following_word", "count"});
-//
-//        TextInputFormat.setInputPaths(job2, args[1]);
-//        TextOutputFormat.setOutputPath(job1, new Path(outputDir));
-//        job2.waitForCompletion(true);
+        Configuration configuration2 = new Configuration();
+        configuration2.set("threshold", threshold);
+        configuration2.set("n", topK);
+
+        DBConfiguration.configureDB(configuration2, "com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/auto_complete", "root", "238604");
+        Job job2 = Job.getInstance(configuration2);
+        job2.setJobName("Model");
+        job2.setJarByClass(Driver.class);
+
+//        job2.addArchiveToClassPath(new Path("path_to_ur_connector"));
+        FileInputFormat.addInputPath(job2, new Path("/home/zzc/.m2/repository/mysql/mysql-connector-java/5.1.39/mysql-connector-java-5.1.39.jar"));
+        job2.setMapperClass(LanguageModel.Map.class);
+        job2.setReducerClass(LanguageModel.Reduce.class);
+
+        job2.setMapOutputKeyClass(Text.class);
+        job2.setMapOutputValueClass(Text.class);
+        job2.setOutputKeyClass(DBOutputWritable.class);
+        job2.setOutputValueClass(NullWritable.class);
+
+        job2.setInputFormatClass(TextInputFormat.class);
+        job2.setOutputFormatClass(DBOutputFormat.class);
+
+        DBOutputFormat.setOutput(job2, "word_count", new String[] {"starting_phrase", "following_word", "count"});
+
+        TextInputFormat.setInputPaths(job2, args[1]);
+        TextOutputFormat.setOutputPath(job1, new Path(outputDir));
+        job2.waitForCompletion(true);
     }
 }
